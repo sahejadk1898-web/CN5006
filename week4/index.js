f7dac5ee17596f657a640c786271fec4cccb4390
sahejadk1898-web <sahejadk1898@gmail.com>
2025-10-22 10:57:29 +0000

    Week4Lab

--- week4/index.js
+++ week4/index.js
@@ -1,1 +1,24 @@
-wee
+
+var express= require("express")
+
+var fs= require("fs")
+
+var app=express()
+
+// add middle ware function for body parsing
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true}));
+app.get('/', function(req,res){
+    res.send("Hello it is my first express application")
+})
+
+app.get('/about', function(req,res)
+{ res.send("This is basic express application from about")})
+
+app.get('/users/:userid/books/:bookid',function(req,res)
+ {res.send(req.params)})
+
+ 
+
+app.listen (5000,function(){console.log("server is running on port 5000")})
+
