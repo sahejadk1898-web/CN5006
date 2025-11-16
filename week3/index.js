968a558cac8a27431e92cee176b6b89d841a2bae
sahejadk1898-web <sahejadk1898@gmail.com>
2025-11-16 15:42:30 +0000

    week4

--- week3/index.js
+++ week3/index.js
@@ -1,1 +1,48 @@
+// defination of the function EmployeeInfo
+function EmployeeInfo(name,Salary)
+{
+console.log("Wellcome " + name+ "Your monthly Salary is "+ Salary)
 
+}
+console.log ("This is my first progame")
+var EmpName="Ali"
+var EmpSalary= 50000
+
+// calling of the function EmployeeInfo
+EmployeeInfo(EmpName,EmpSalary)
+
+//Code for Second Exercise starts from here:
+const EmpSkills= (skills)=> {
+ console.log("Expert in "+ skills)
+}
+EmpSkills("java")
+const student= require('./StudentInfo')
+const person = require('./Person')
+
+// because getName is the function so we use ()
+console.log("Student Name:" +student.getName())
+console.log(student.Location())
+console.log(student.dob)
+
+// because dob is a variable so we do nt use ()
+console.log(student.Studentgrade())
+console.log("grade is "+student.Studentgrade(55) )
+
+// creating new Person
+person1= new person("Jim","USA","myemail@gmail.com")
+console.log("using Person Module",person1.getPersonInfo())
+console.log("Programe ended")
+
+// Exercise4.js
+os=require("os")
+const util=require('util')
+console.log("temporary directory"+ os.tmpdir() )
+console.log("hostname: "+ os.hostname())
+console.log("OS : " + os.platform() +"release:"+ os.release())
+console.log("Uptime"+ (os.uptime())/3600 +" hours")
+console.log("userInfo" + util.inspect(os.userInfo()))
+console.log("Memory "+ os.totalmem()/1000000000 + "Giga byte")
+console.log(" free: "+os.freemem()/1000000000 + "Giga byte")
+console.log("CPU "+ util.inspect(os.cpus()))
+console.log("Network"+ util.inspect(os.networkInterfaces()))
+console.log("programe end")
--- week4/index.js
+++ week4/index.js
@@ -15,10 +15,38 @@ app.get('/', function(req,res){
 app.get('/about', function(req,res)
 { res.send("This is basic express application from about")})
 
-app.get('/users/:userid/books/:bookid',function(req,res)
+app.get(about/'/users/:userid/books/:bookid',function(req,res)
  {res.send(req.params)})
-
- 
 
 app.listen (5000,function(){console.log("server is running on port 5000")})
 
+app.get('/about',function(req,res)
+{ res.send("This is basic express application ")
+})
+app.get('/users/:userId/books/:bookId', function (req, res) { res.send(req.params)
+})
+
+app.get('/GetStudents',function (req,res)
+ { studentdata={}
+ fs.readFile(__dirname + "/" + "Student.json", 'utf8', function (err,
+data) { console.log( data );
+ res.json({ 'status':true, 'Status_Code':200,
+ 'requested at': req.localtime, 'requrl':req.url,
+ 'request Method':req.method, 'studentdata':JSON.parse(data)});
+});
+}) 
+
+app.get('/GetStudentid/:id',(req,res)=>{
+ studentdata={}
+ fs.readFile(__dirname + "/" + "Student.json", 'utf8', function (err, data) {
+ var students= JSON.parse(data)
+ var student=students["Student"+req.params.id]
+ console.log("student",student)
+ if (student)
+ res.json(student)
+ else
+ res.json({ 'status':true, 'Status_Code':200,
+ 'requested at': req.localtime, 'requrl':req.url, 
+  'request Method':req.method, 'studentdata':JSON.parse(data)});
+ });
+ })
