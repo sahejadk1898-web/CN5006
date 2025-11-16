b3dc364ca499a85db9c20db940c2e549f9c74206
sahejadk1898-web <sahejadk1898@gmail.com>
2025-11-16 20:19:49 +0000

    week4

--- week4/index.js
+++ week4/index.js
@@ -1,30 +1,20 @@
-
-var express= require("express")
-
+var express=require("express")
 var fs= require("fs")
-
 var app=express()
-
 // add middle ware function for body parsing
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true}));
-app.get('/', function(req,res){
-    res.send("Hello it is my first express application")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/',function(req,res){
+res.send("hello it is my first express application")
 })
-
-app.get('/about', function(req,res)
-{ res.send("This is basic express application from about")})
-
-app.get(about/'/users/:userid/books/:bookid',function(req,res)
- {res.send(req.params)})
-
-app.listen (5000,function(){console.log("server is running on port 5000")})
+app.listen(5000,function(){console.log("server is running on port 5000")})
 
 app.get('/about',function(req,res)
-{ res.send("This is basic express application ")
+{ res.send("This is basic express application From About ")
 })
 app.get('/users/:userId/books/:bookId', function (req, res) { res.send(req.params)
 })
+
 
 app.get('/GetStudents',function (req,res)
  { studentdata={}
@@ -35,6 +25,7 @@ data) { console.log( data );
  'request Method':req.method, 'studentdata':JSON.parse(data)});
 });
 }) 
+
 
 app.get('/GetStudentid/:id',(req,res)=>{
  studentdata={}
@@ -50,3 +41,21 @@ app.get('/GetStudentid/:id',(req,res)=>{
   'request Method':req.method, 'studentdata':JSON.parse(data)});
  });
  })
+
+
+ app.get('/studentinfo',function(req,res)
+{
+res.sendFile('StudentInfo.html', { root: __dirname });
+})
+app.post('/submit-data', function (req, res) {
+var name = req.body.firstName + ' ' + req.body.lastName+ ' ';
+var Age= req.body.myAge+ ' Gender: ' + req.body.gender+ '';
+Qual= ' Qualification'+ req.body.Qual;
+console.log(req.body.Qual)
+res.send({
+status: true, 
+message: 'form Details', data: {
+name: name, age:Age, Qualification:Qual,
+}
+});
+});
