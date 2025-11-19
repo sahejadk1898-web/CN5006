db3418bb82b84529a8e42a2b4b42b907b9831112
sahejadk1898-web <sahejadk1898@gmail.com>
2025-11-19 11:54:35 +0000

    week8

--- week8/index.js
+++ week8/index.js
@@ -1,1 +1,46 @@
+mongoose = require('mongoose');
+//app = express();
+const MONGO_URI = 'mongodb+srv://sahejadali9822_db_user:BXMf5UQBDIMGFolV@cluster0.bjexndi.mongodb.net/week8';
+mongoose.connect(MONGO_URI, {useUnifiedTopology: true,useNewUrlParser: true})
+; const db = mongoose.connection;
+db.on('error', function(err)
+{console.log("Error occured during connection"+err)
+}
+);
+db.once('connected', function() {
+console.log(`Connected to ${MONGO_URI}`);
+});
+// creating the scheme
+const PersonScheme = new mongoose.Schema({ name: {
+type: String, required: true
+},
+age: Number, Gender:String, Salary:Number
+});
+// creating model named as modelname with collection named as personCollection
+const person_doc = mongoose.model('modelname', PersonScheme,'personCollection');
+// creating a single document
+const doc1 = new person_doc({ name: 'Ali',age:22,Gender:"Male",Salary:100000 }
+);
+// adding one document in the collection
+doc1
+ .save()
+ .then((doc1) => {
+ console.log("New Article Has been Added Into Your DataBase.",doc1);
+ })
+ .catch((err) => {
+ console.error(err);
+ });
 
+//Adding multiple documents
+ manypersons=[{ name: 'Simon',age:42,Gender:"Male",Salary:3456 }
+ ,{ name: 'Neesha',age:23,Gender:"Female",Salary:1000 }
+ ,{ name: 'Mary',age:27,Gender:"Female",Salary:5402 },
+ { name: 'Mike',age:40,Gender:"Male",Salary:4519 }
+ ]
+
+
+ person_doc.insertMany(manypersons).then(function(){
+ console.log("Data inserted") // Success
+ }).catch(function(error){
+ console.log(error) // Failure
+ }); 
